1994b80f4e387f4be1dcf06da62937094af79dee
Colin Trezise <ctrezise@asu.edu>
2022-11-26 14:32:35 -0700

    refactored procs and types into their own module

--- src/snake.nim
+++ src/snake.nim
@@ -1,75 +1,5 @@
-import os, deques, random, unicode
-import illwill
-
-proc exitProc() {.noconv.} =
-  illwillDeinit()
-  showCursor()
-  quit(0)
-
-type
-  ScreenInfo = object
-    screenWidth: int
-    screenHeight: int
-    midX: int
-    midY: int
-
-type
-  BoardInfo = object
-    boardWidth: int
-    boardHeight: int
-    upperLeftX: int
-    upperLeftY: int
-    bottomRightX: int
-    bottomRightY: int
-
-type
-  GameInfo = object
-    screenInfo: ScreenInfo
-    boardInfo: BoardInfo
-
-type
-  Position = object
-    x: int
-    y: int
-
-type
-  Direction = enum
-    left, right, up, down
-
-type
-  MovementResult = enum
-    died, ate, moved
-
-proc directionAsPosition(direction: Direction): Position =
-  case direction:
-    of Direction.up:
-      return Position(x: 0, y: -1)
-    of Direction.down:
-      return Position(x: 0, y: 1)
-    of Direction.right:
-      return Position(x: 1, y: 0)
-    of Direction.left:
-      return Position(x: -1, y: 0)
-
-proc youDied(tb: var TerminalBuffer, screenInfo: ScreenInfo) =
-  let offset = 4
-  tb.write(screenInfo.midX-offset, screenInfo.midY, fgRed, "YOU DIED")
-  tb.display()
-  sleep(1000)
-
-proc findFoodSpot(boardInfo: BoardInfo): Position =
-  let foodX = rand(boardInfo.upperLeftX+1 .. boardInfo.bottomRightX-1)
-  let foodY = rand(boardInfo.upperLeftY+1 .. boardInfo.bottomRightY-1)
-  return Position(x:foodX, y:foodY)
-
-proc placeFood(boardInfo: BoardInfo, tb: var TerminalBuffer): Position =
-  var food = findFoodSpot(boardInfo)
-  var foodSpot = tb[food.x, food.y].ch
-  while foodSpot == Rune('@') or foodSpot == Rune('#'):
-    food = findFoodSpot(boardInfo)
-    foodSpot = tb[food.x, food.y].ch
-  tb.write(food.x, food.y, fgCyan, "*")
-  return food
+import os, deques, random, unicode, illwill
+import procsAndTypes
 
 const BOARD_WIDTH = 50
 const BOARD_HEIGHT = 15
@@ -114,7 +44,6 @@ var movement = Direction.right
 #add food
 var food = placeFood(boardInfo, tb)
 
-# Main event loop
 while true:
   # display score
   tb.write(boardInfo.upperLeftX, boardInfo.upperLeftY - 1, fgWhite, "Score: " & $score)
@@ -139,7 +68,6 @@ while true:
       # snake has ran into some food and should get bigger
       score = score + 1
       tb.write(newHead.x, newHead.y, fgGreen, "@")
-      #generate new food
       food = placeFood(boardInfo, tb)
     else:
       #snake just moved with no special event
